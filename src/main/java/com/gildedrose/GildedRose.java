d84c1faad03019f2708e771657f70db00ada325c
CatalinaRadion <radioncatalina@gmail.com>
2022-10-29 16:09:15 +0300

    Refactored Version

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,8 +1,8 @@
 package com.gildedrose;
 
-import java.util.Arrays;
+import com.gildedrose.exceptions.InvalidItemException;
+import com.gildedrose.items.*;
 
-import static java.util.Arrays.asList;
 
 class GildedRose {
     Item[] items;
@@ -11,66 +11,26 @@ class GildedRose {
         this.items = items;
     }
 
-    public void update() {
-        Arrays.stream(items).forEach(this::performUpdate);
-    }
-
-    private void performUpdate(Item item) {
-        updateQuality(item);
-        updateSellIn(item);
-
-    }
-
-    private void updateQuality(Item item) {
-        if(asList("+5 Dexterity Vest", "Elixir of the Mongoose", "Conjured Mana Cake").contains(item.name)) {
-            alterQualityConsideringSellIn(item, -1);
-        }
-
-        if(asList("Aged Brie").contains(item.name)) {
-            alterQualityConsideringSellIn(item, 1);
-        }
-
-        if(asList("Backstage passes to a TAFKAL80ETC concert").contains(item.name)) {
-
-            item.quality = item.quality+1;
-
-            if(item.sellIn <= 10) {
-                item.quality = item.quality+1;
-            }
-            if(item.sellIn <= 5) {
-                item.quality = item.quality+1;
-            }
-            if(item.sellIn == 0) {
-                item.quality = 0;
-            }
+    public void updateQuality() throws InvalidItemException {
+        for (Item item : items) {
+            NormalItem itemPouch = categorize(item);
+            itemPouch.verifyItemIntegrity(item);
+            itemPouch.updateItem(item);
         }
     }
 
-    private void alterQualityConsideringSellIn(Item item, int qualityModifiedValue) {
-        if(item.sellIn > 0 ) {
-            item.quality = item.quality + qualityModifiedValue;
-        } else {
-            item.quality = item.quality + 2*qualityModifiedValue;
+    private NormalItem categorize(Item item) {
+        if ("Sulfuras, Hand of Ragnaros".equals(item.name)) {
+            return new LegendaryItem();
         }
-    }
-
-    private void updateSellIn(Item item) {
-        if(!"Sulfuras, Hand of Ragnaros".equals(item.name)) {
-            item.sellIn--;
-            maximumQuality(item);
-            minimumQuality(item);
+        if ("Aged Brie".equals(item.name)) {
+            return new Brie();
         }
-    }
-
-    private void maximumQuality(Item item) {
-        if(item.quality > 50 ) {
-            item.quality = 50;
+        if ("Backstage passes to a TAFKAL80ETC concert".equals(item.name)) {
+            return new BackstagePass();
         }
+        if (item.name.startsWith("Conjured"))
+            return new ConjuredItem();
+        return new NormalItem();
     }
-    private void minimumQuality(Item item) {
-        if(item.quality < 0 ) {
-            item.quality = 0;
-        }
-    }
-
 }
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,105 +1,177 @@
 package com.gildedrose;
 
+import com.gildedrose.exceptions.InvalidItemException;
+import com.gildedrose.items.Item;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class GildedRoseTest {
 
-//    @Test
-//    void foo() {
-//        Item[] items = new Item[] { new Item("foo", 0, 0) };
-//        GildedRose app = new GildedRose(items);
-//        app.updateQuality();
-//        assertEquals("fixme", app.items[0].name);
-//    }
-
     @Test
-    void test5Dexterity() {
-        Item[] items = new Item[] { new Item("+5 Dexterity Vest", 2, 2),
-            new Item("+5 Dexterity Vest", 0, 4),
-            new Item("+5 Dexterity Vest", 3, 51),
-            new Item("+5 Dexterity Vest", 3, 0)};
+    public void updateQualityDecrementsItemsQualityAndSellInBy1() throws InvalidItemException {
+        Item[] items = new Item[]{new Item("Elixir of the Mongoose", 1, 1)};
         GildedRose app = new GildedRose(items);
-        app.update();
-        assertEquals(1 , app.items[0].sellIn);
-        assertEquals(1 , app.items[0].quality);
-        assertEquals(-1 , app.items[1].sellIn);
-        assertEquals(2 , app.items[1].quality);
-        assertEquals(2 , app.items[2].sellIn);
-        assertEquals(50 , app.items[2].quality);
-        assertEquals(2 , app.items[3].sellIn);
-        assertEquals(0 , app.items[3].quality);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+        assertEquals(0, app.items[0].sellIn);
     }
 
     @Test
-    void testAgedBrie() {
-        Item[] items = new Item[] { new Item("Aged Brie", 0, 4),
-                                    new Item("Aged Brie", 3, 4),
-            new Item("Aged Brie", 3, 50)};
+    public void onceTheSellByDateHasPassedQualityDegradesTwiceAsFast() throws InvalidItemException {
+        Item[] items = new Item[]{new Item("Elixir of the Mongoose", -1, 2)};
         GildedRose app = new GildedRose(items);
-        app.update();
-        assertEquals(-1 , app.items[0].sellIn);
-        assertEquals(6 , app.items[0].quality); // sellIn < 0 -> quality +2
-        assertEquals(2 , app.items[1].sellIn);
-        assertEquals(5 , app.items[1].quality);
-        assertEquals(2 , app.items[2].sellIn);
-        assertEquals(50 , app.items[2].quality);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+        assertEquals(-2, app.items[0].sellIn);
     }
 
     @Test
-    void testElixir() {
-        Item[] items = new Item[] { new Item("Elixir of the Mongoose", 0, 4),
-            new Item("Elixir of the Mongoose", 3, 4)};
+    public void qualityOfAnItemCanNeverBecomeNegative() throws InvalidItemException {
+        Item[] items = new Item[]{new Item("Elixir of the Mongoose", 2, 2)};
         GildedRose app = new GildedRose(items);
-        app.update();
-        assertEquals(-1 , app.items[0].sellIn);
-        assertEquals(2 , app.items[0].quality);
-        assertEquals(2 , app.items[1].sellIn);
-        assertEquals(3 , app.items[1].quality);
+        app.updateQuality();
+        app.updateQuality();
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
     }
 
     @Test
-    void testSulfuras() {
-        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 4),
-            new Item("Sulfuras, Hand of Ragnaros", 3, 4)};
+    public void theQualityOfAnItemCanNeverBecomeMoreThan50() throws InvalidItemException {
+        Item[] items = new Item[]{new Item("Aged Brie", -1, 50), new Item("Backstage passes to a TAFKAL80ETC concert", 3, 49)};
         GildedRose app = new GildedRose(items);
-        app.update();
-        assertEquals(0 , app.items[0].sellIn);
-        assertEquals(4 , app.items[0].quality);
-        assertEquals(3 , app.items[1].sellIn);
-        assertEquals(4 , app.items[1].quality);
+        app.updateQuality();
+
+        assertEquals(50, app.items[0].quality);
+        assertEquals(-2, app.items[0].sellIn);
+
+        assertEquals(50, app.items[1].quality);
+        assertEquals(2, app.items[1].sellIn);
     }
 
     @Test
-    void testBackstagePasses() {
-        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 4),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 3, 4),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 9, 4),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 11, 4)};
+    public void briesQualityIncrementsByOneForEachDayBeforeItsSellByDate() throws InvalidItemException {
+        Item[] items = new Item[]{new Item("Aged Brie", 4, 1)};
         GildedRose app = new GildedRose(items);
-        app.update();
-        assertEquals(-1 , app.items[0].sellIn);
-        assertEquals(0 , app.items[0].quality); // sellIn < 0 -> quality +2
-        assertEquals(2 , app.items[1].sellIn);
-        assertEquals(7 , app.items[1].quality);
-        assertEquals(8 , app.items[2].sellIn);
-        assertEquals(6 , app.items[2].quality);
-        assertEquals(10 , app.items[3].sellIn);
-        assertEquals(5 , app.items[3].quality);
+        app.updateQuality();
+        assertEquals(2, app.items[0].quality);
+        assertEquals(3, app.items[0].sellIn);
     }
 
     @Test
-    void testConjuredManaCake() {
-        Item[] items = new Item[] { new Item("Conjured Mana Cake", 0, 4),
-            new Item("Conjured Mana Cake", 3, 4)};
+    public void briesQualityIncrementsByTwoForEachDayPastItsSellByDate() throws InvalidItemException {
+        Item[] items = new Item[]{new Item("Aged Brie", -1, 1)};
         GildedRose app = new GildedRose(items);
-        app.update();
-        assertEquals(-1 , app.items[0].sellIn);
-        assertEquals(2 , app.items[0].quality);
-//        assertEquals(0 , app.items[0].quality);
-        assertEquals(2 , app.items[1].sellIn);
-        assertEquals(3 , app.items[1].quality);
-//        assertEquals(2 , app.items[1].quality);
+        app.updateQuality();
+        assertEquals(3, app.items[0].quality);
+        assertEquals(-2, app.items[0].sellIn);
     }
+
+    @Test
+    public void SulfurasBeingALegendaryItemNeverHasToBeSoldOrDecreasesInQuality() throws InvalidItemException {
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", -1, 80)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(80, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
+    }
+
+    @Test
+    public void backstagePassesQualityIncrementsByOneWithEachDayPassing() throws InvalidItemException {
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 11, 30)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(31, app.items[0].quality);
+        assertEquals(10, app.items[0].sellIn);
+    }
+
+    @Test
+    public void backstagePassesIncreaseInQualityBy2WhenThereAre10DaysOrLessRemaining() throws InvalidItemException {
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 10, 30)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(32, app.items[0].quality);
+        assertEquals(9, app.items[0].sellIn);
+    }
+
+    @Test
+    public void backstagePassesIncreaseInQualityBy3WhenThereAre5DaysOrLessRemaining() throws InvalidItemException {
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 5, 33)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(36, app.items[0].quality);
+        assertEquals(4, app.items[0].sellIn);
+    }
+
+    @Test
+    public void backstagePassesQualityDropsTo0AfterTheConcert() throws InvalidItemException {
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", -1, 30)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+        assertEquals(-2, app.items[0].sellIn);
+    }
+
+    @Test
+    public void conjuredItemsDegradeInQualityTwiceAsFastAsNormalItems() throws InvalidItemException {
+        Item[] items = new Item[]{new Item("Conjured", 2, 30)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(28, app.items[0].quality);
+        assertEquals(1, app.items[0].sellIn);
+    }
+
+    @Test
+    public void conjuredItemsDegradeInQualityTwiceAsFastAsNormalItemsWhenPastItsSellByDate() throws InvalidItemException {
+        Item[] items = new Item[]{new Item("Conjured", -1, 30)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(26, app.items[0].quality);
+        assertEquals(-2, app.items[0].sellIn);
+    }
+
+    @Test
+    void testItemIntegrityIfItemStartsAbove50QualityAndIsNotLegendary() {
+        InvalidItemException exception = null;
+        Item[] items = new Item[]{new Item("+5 Dexterity Vest", 3, 51)};
+        GildedRose app = new GildedRose(items);
+        try {
+            app.updateQuality();
+        } catch (InvalidItemException e) {
+            exception = e;
+        }
+        assertNotNull(exception);
+        assertEquals(exception.getMessage(), "Item quality can not start above 50");
+    }
+
+    @Test
+    void testItemIntegrityIfItemStartsBelow0Quality() {
+        InvalidItemException exception = null;
+        Item[] items = new Item[]{new Item("+5 Dexterity Vest", 3, -1)};
+        GildedRose app = new GildedRose(items);
+        try {
+            app.updateQuality();
+        } catch (InvalidItemException e) {
+            exception = e;
+        }
+        assertNotNull(exception);
+        assertEquals(exception.getMessage(), "Item quality can not start below 0");
+    }
+
+    @Test
+    void testItemIntegrityIfLegendaryItemStartsWithQualityDifferentThan80() {
+        InvalidItemException exception = null;
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", -1, 50)};
+        GildedRose app = new GildedRose(items);
+        try {
+            app.updateQuality();
+        } catch (InvalidItemException e) {
+            exception = e;
+        }
+        assertNotNull(exception);
+        assertEquals(exception.getMessage(), "Legendary items should have 80 quality");
+    }
+
 }
